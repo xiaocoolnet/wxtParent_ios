05924c0b5d007a81d1ab55d5bcca832aa5ed0a3f
李春波 <lichunbo@lichunbodemacbook-air.local>
2016-01-16 18:01:10 +0800

    2016年1月16日_2

---
+++ Third Package/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  WXT_Parents
+//
+//  Created by 李春波 on 15/12/28.
+//  Copyright © 2015年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
