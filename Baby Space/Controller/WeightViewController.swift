c65c81aa716f91e7328cd956abece816cbced1a7
李春波 <wowowo110nb@163.com>
2016-02-16 18:06:05 +0800

    2016年2月16日 新增体重图表视图Demo

--- Baby Space/Controller/WeightViewController.swift
+++ Baby Space/Controller/WeightViewController.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import SwiftCharts
 
 class WeightViewController: UIViewController {
 
+    private var chart: Chart?
+    @IBOutlet weak var weightView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let labelSettings = ChartLabelSettings(font: ExamplesDefaults.labelFont)
+        //(1, 22), (3, 23), (5, 24), (6, 25), (8, 25), (9, 26), (10, 26)
+        let chartPoints = [(1, 22), (3, 23), (5, 24), (6, 25), (8, 25), (9, 26), (10, 26)].map{ChartPoint(x: ChartAxisValueInt($0.0), y: ChartAxisValueInt($0.1))}
+        
+        let xValues = Array(1.stride(through: 12, by: 1)).map {ChartAxisValueInt($0, labelSettings: labelSettings)}
+        let yValues = Array(20.stride(through: 30, by: 2)).map {ChartAxisValueInt($0, labelSettings: labelSettings)}
+        
+        let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "月份", settings: labelSettings))
+        let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "体重", settings: labelSettings))
+        //let chartFrame = ExamplesDefaults.chartFrame(0,0,300,200)
+        
+        let chartSettings = ExamplesDefaults.chartSettings
+        let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: CGRectMake(1, 1, 300, 200), xModel: xModel, yModel: yModel)
+        let (xAxis, yAxis, innerFrame) = (coordsSpace.xAxis, coordsSpace.yAxis, coordsSpace.chartInnerFrame)
+        
+        // create layer with line
+        let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor(red: 0.4, green: 0.4, blue: 1, alpha: 0.2), lineWidth: 3, animDuration: 0.7, animDelay: 0)
+        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [lineModel])
+        
+        // view generator - creates circle view for each chartpoint
+        let circleViewGenerator = {(chartPointModel: ChartPointLayerModel, layer: ChartPointsLayer, chart: Chart) -> UIView? in
+            let circleView = ChartPointEllipseView(center: chartPointModel.screenLoc, diameter: 6)
+            circleView.animDuration = 0.5
+            circleView.fillColor = UIColor.redColor()
+            return circleView
+        }
+        // create layer that uses the view generator
+        let chartPointsCircleLayer = ChartPointsViewsLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, viewGenerator: circleViewGenerator, displayDelay: 0, delayBetweenItems: 0.05)
+        
+        // create layer with guidelines
+        //var settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: ExamplesDefaults.guidelinesWidth, axis: ChartDividersLayerAxis = .XAndY)
+        let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: ExamplesDefaults.guidelinesWidth, dotWidth: 1.0, dotSpacing: 1.0)
+        let guidelinesLayer = ChartGuideLinesDottedLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, settings: settings)
+        
+        let chart = Chart(
+            frame: CGRectMake(0, 0, 300, 200),
+            layers: [
+                xAxis,
+                yAxis,
+                guidelinesLayer,
+                chartPointsLineLayer,
+                chartPointsCircleLayer
+            ]
+        )
+        
+        self.weightView.addSubview(chart.view)
+        self.chart = chart        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
